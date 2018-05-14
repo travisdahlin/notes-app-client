b2c6e107236200d7218fb7ac3dcb202bd2a0e261
Travis Dahlin <trdahlin@gmail.com>
2018-05-13 20:20:23 -0700

    login add login state.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,24 @@ import Routes from './Routes'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isAuthenticated: false
+    }
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated })
+  }
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    }
+
     return (
       <div className="App container">
         <Navbar fluid collapseOnSelect>
@@ -27,7 +44,7 @@ class App extends Component {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Routes />
+        <Routes childProps={childProps} />
       </div>
     )
   }
