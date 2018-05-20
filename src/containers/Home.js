30e47fd692cc96eb76f8e8c866b227fefd10a20e
Travis Dahlin <trdahlin@gmail.com>
2018-05-19 19:57:32 -0700

    list-notes add a method to display notes on Home
    The Home page will display the landing page if not authenticated and will display a users notes if if authenticated.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react'
+import { PageHeader, ListGroup } from 'react-bootstrap'
 import './Home.css'
 
 export default class Home extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoading: true,
+      notes: []
+    }
+  }
+
+  renderLander() {
     return (
       <div className="Home">
         <div className="lander">
           <h1>Scratch</h1>
           <p>A simple note taking app</p>
         </div>      
+      </div>
+    )
+  }
+
+  renderNotes() {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          {!this.state.isLoading && this.renderNotesList(this.state.notes)}
+        </ListGroup>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        {this.props.isAuthenticated ? this.renderNotes() : this.renderLander()}
       </div>
     )
   }
