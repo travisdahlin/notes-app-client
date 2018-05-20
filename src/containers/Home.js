0186e2b711ea819d6d7032b971e7d8699f48ba24
Travis Dahlin <trdahlin@gmail.com>
2018-05-19 20:47:43 -0700

    list-notes add method to retrieve notes.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { PageHeader, ListGroup } from 'react-bootstrap'
+import { PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { API } from 'aws-amplify'
 import './Home.css'
 
 export default class Home extends Component {
@@ -10,6 +11,55 @@ export default class Home extends Component {
       isLoading: true,
       notes: []
     }
+  }
+
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return
+    }
+
+    try {
+      const notes = await this.notes()
+      this.setState({ notes })
+    }
+    catch (e) {
+      alert(e)
+    }
+
+    this.setState({ isLoading: false })
+  }
+
+  notes() {
+    return API.get('notes', '/notes')
+  }
+
+  renderNotesList(notes) {
+    return [{}].concat(notes).map(
+      (note, i) =>
+        i !== 0
+          ? <ListGroupItem
+              key={note.noteId}
+              href={`/notes/${note.noteId}`}
+              onClick={this.handleNoteClick}
+              header={note.content.trim().split('\n')[0]}
+            >
+              {"Created: " + new Date(note.createdAt).toLocaleString()}
+            </ListGroupItem>
+          : <ListGroupItem
+              key="new"
+              href="/notes/new"
+              onClick={this.handleNoteClick}
+            >
+              <h4>
+                <b>{"\uFF0B"}</b> Create a new note
+              </h4>
+            </ListGroupItem>
+    )
+  }
+
+  handleNoteClick = event => {
+    event.preventDefault()
+    this.props.history.push(event.currentTarget.getAttribute('href'))
   }
 
   renderLander() {
