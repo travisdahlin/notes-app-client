061038f73af17caf19438a6bbd5943f11d2255e1
Travis Dahlin <trdahlin@gmail.com>
2018-05-22 22:06:36 -0700

    list-notes save changes to notes

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { API, Storage } from 'aws-amplify'
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import { s3Upload } from '../libs/awsLib'
+import LoaderButton from '../components/LoaderButton'
+import config from '../config'
+import './Notes.css'
 
 export default class Notes extends Component {
 	constructor(props) {
@@ -7,6 +12,8 @@ export default class Notes extends Component {
 
     this.file = null
     this.state = {
+      isLoading: null,
+      isDeleting: null,
       note: null,
       content: '',
       attachmentURL: null
@@ -38,7 +45,124 @@ export default class Notes extends Component {
     return API.get("notes", `/notes/${this.props.match.params.id}`)
   }
 
+  validateForm() {
+    return this.state.content.length > 0
+  }
+
+  formatFilename(str) {
+    return str.replace(/^\w+-/, "")
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  handleFileChange = event => {
+    this.file = event.target.files[0]
+  }
+
+  saveNote(note) {
+    return API.put("notes", `/notes/${this.props.match.params.id}`, {
+      body: note
+    })
+  }
+
+  handleSubmit = async event => {
+    let attachment
+    
+    event.preventDefault()
+
+    if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
+      alert('Please pick a file smaller than 5MB')
+      return
+    }
+
+    this.setState({ isLoading: true })
+
+    try {
+      if (this.file) {
+        attachment = await s3Upload(this.file)
+      }
+
+      await this.saveNote({
+        content: this.state.content,
+        attachment: attachment || this.state.attachment
+      })
+      this.props.history.push("/")
+    }
+    catch (e) {
+      alert (e)
+      this.setState({ isLoading: false })
+    }
+  }
+
+  handleDelete = async event => {
+    event.preventDefault()
+
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this note?"
+    )
+
+    if (!confirmed) {
+      return
+    }
+
+    this.setState({ isDeleting: true })
+  }
+
   render() {
-    return <div className="Notes"></div>
+    return (
+      <div className="Notes">
+        {this.state.note &&
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="content">
+              <FormControl
+                onChange={this.handleChange}
+                value={this.state.content}
+                componentClass="textarea"
+              />
+            </FormGroup>
+            {this.state.note.attachment &&
+              <FormGroup>
+                <ControlLabel>Attachment</ControlLabel>
+                <FormControl.Static>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={this.state.attachmentURL}
+                  >
+                    {this.formatFilename(this.state.note.attachment)}
+                  </a>
+                </FormControl.Static>
+              </FormGroup>}
+            <FormGroup controlId="file">
+              {!this.state.note.attachment &&
+                <ControlLabel>Attachment</ControlLabel>}
+              <FormControl onChange={this.handleFileChange} type="file" />
+            </FormGroup>
+            <LoaderButton
+              block
+              bsStyle="primary"
+              bsSize="large"
+              disabled={!this.validateForm()}
+              type="submit"
+              isLoading={this.state.isLoading}
+              text="Save"
+              loadingText="Saving…"
+            />
+            <LoaderButton
+              block
+              bsStyle="danger"
+              bsSize="large"
+              isLoading={this.state.isDeleting}
+              onClick={this.handleDelete}
+              text="Delete"
+              loadingText="Deleting…"
+            />
+          </form>}
+      </div>
+    );
   }
 }
